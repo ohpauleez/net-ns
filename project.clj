d13c20cec7aaf3402dcc4a7ef7adddfce35f120e
Paul deGrandis <paul.degrandis@gmail.com>
2010-10-14 20:34:31 -0700

    simple cleanup

--- project.clj
+++ project.clj
@@ -5,14 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "See the notice in README.md or details in LICENSE.html"}
-  :dependencies [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
-                 ;[org.clojure/clojure "1.3.0-alpha1"]
+  :dependencies [;[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
+                 [org.clojure/clojure "1.3.0-alpha1"]
                  [aleph "0.1.1-SNAPSHOT"]]
   :dev-dependencies [[vimclojure/server "2.2.0-SNAPSHOT"]
                      ;[lein-difftest "1.3.1"]
                      ;[swank-clojure "1.3.0-SNAPSHOT"]
                      [org.clojars.ninjudd/lazytest "1.1.3-SNAPSHOT"]
-                     [org.clojure.contrib/repl-utils "1.3.0-SNAPSHOT"]]
+                     ;[org.clojure.contrib/repl-utils "1.3.0-SNAPSHOT"]
+                     [org.clojure.contrib/repl-utils "1.3.0-alpha1"]]
                      ;[lein-run "1.0.0-SNAPSHOT"]
                      ;[radagast "1.0.0"]
   ;:warn-on-reflection true
