c8955efb9d9b462945e97940b319342af080d689
Paul deGrandis <paul.degrandis@gmail.com>
2010-10-28 15:58:28 -0700

    Moving along to the clj 1.3 alpha2, project.clj changes

--- project.clj
+++ project.clj
@@ -6,15 +6,15 @@
             :distribution :repo
             :comments "See the notice in README.md or details in LICENSE.html"}
   :dependencies [;[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
-                 [org.clojure/clojure "1.3.0-alpha1"]
+                 [org.clojure/clojure "1.3.0-alpha2"]
                  [aleph "0.1.1-SNAPSHOT"]]
-  :dev-dependencies [[vimclojure/server "2.2.0-SNAPSHOT"]
+  :dev-dependencies [[vimclojure/server "2.2.0"]
                      ;[lein-difftest "1.3.1"]
                      ;[swank-clojure "1.3.0-SNAPSHOT"]
                      [org.clojars.ninjudd/lazytest "1.1.3-SNAPSHOT"]
                      ;[org.clojure.contrib/repl-utils "1.3.0-SNAPSHOT"]
-                     [org.clojure.contrib/repl-utils "1.3.0-alpha1"]
-                     [org.clojure.contrib/core "1.3.0-alpha1"]]
+                     [org.clojure.contrib/repl-utils "1.3.0-alpha2"]
+                     [org.clojure.contrib/core "1.3.0-alpha2"]]
                      ;[lein-run "1.0.0-SNAPSHOT"]
                      ;[radagast "1.0.0"]
   ;:warn-on-reflection true
