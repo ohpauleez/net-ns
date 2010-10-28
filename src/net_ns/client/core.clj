641f8a92e9808d44596762592dba14f944506eaf
Paul deGrandis <paul.degrandis@gmail.com>
2010-10-28 16:01:40 -0700

    Adding some stubbing and notes for client side

---
+++ src/net_ns/client/core.clj
@@ -0,0 +1,28 @@
+(ns net-ns.client.core
+  (:require [clojure.repl :as clj-repl]
+            [clojure.string :as clj-str]))
+
+(comment
+  (net-ns
+    :register [some-func :to ["host1" "192.168.2.2" :as this-func]
+                             ["host3"]
+                             ["host4" :as some-function]]
+    :expose [:to ["host1]
+                 ["host2" :only some-func))
+(comment (expose function & to-lists))
+
+(defn registerfn-str
+  ""
+  ;([f & to-specs])
+  [f ns-info & to-specs]
+  (str "reg" (hash-map :to to-specs :ns ns-info) ": " (clj-repl/source f)))
+
+; ns-map -> map of all the ns mappings
+; ns-publics -> all public interns for the ns
+; ns-interns -> all interns for the ns
+
+(defn map-replace-str
+  "Perform a bunch of replaces on a string, using a map {'match-word' 'new-word'"
+  [s str-map]
+  (reduce (fn [ss sub] (clj-str/replace ss (sub 0) (sub 1)) s (vec str-map))))
+
