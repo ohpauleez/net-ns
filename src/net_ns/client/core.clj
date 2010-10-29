290438d8c78dde629695366e35abb47efdc1bcc7
Paul deGrandis <paul.degrandis@gmail.com>
2010-10-29 11:13:10 -0700

    Cleaned up the net-ns example in the comment

--- src/net_ns/client/core.clj
+++ src/net_ns/client/core.clj
@@ -4,11 +4,13 @@
 
 (comment
   (net-ns
-    :register [some-func :to ["host1" "192.168.2.2" :as this-func]
-                             ["host3"]
-                             ["host4" :as some-function]]
-    :expose [:to ["host1]
-                 ["host2" :only some-func))
+    (:register [some-func :to ["host1" "192.168.2.2" :as this-func]
+                              ["host3"]
+                              ["host4" :as some-function]])
+    (:expose [:to ["host1]
+                  ["host2" :only some-func]])
+    (:require [some-other-func :as this-cool-func]
+              [one-more-func :as omf :from "host1"])))
 (comment (expose function & to-lists))
 
 (defn registerfn-str
@@ -20,6 +22,7 @@
 ; ns-map -> map of all the ns mappings
 ; ns-publics -> all public interns for the ns
 ; ns-interns -> all interns for the ns
+; ns-resolve -> resolves the symbol the given namespace, nil if not resolved
 
 (defn map-replace-str
   "Perform a bunch of replaces on a string, using a map {'match-word' 'new-word'"
